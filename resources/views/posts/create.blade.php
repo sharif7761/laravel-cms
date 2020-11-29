6edc7fbf4c61c59a9b919e6d9fdd36f264e2b7f9
sharif7761 <sharif7761@gmail.com>
2020-11-30 01:20:09 +0600

    Adding datepicker and trix editor

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -31,8 +31,9 @@
                     <textarea class="form-control" name="description" id="description" cols="5" rows="5"></textarea>
                 </div>
                 <div class="form-group">
-                    <label for="content">Content</label>
-                    <textarea class="form-control" name="contents" id="contents" cols="5" rows="5"></textarea>
+                    <label for="contents">Content</label>
+                    <input id="contents" type="hidden" name="contents">
+                    <trix-editor input="contents"></trix-editor>
                 </div>
                 <div class="form-group">
                     <label for="published_at">Published At</label>
@@ -50,4 +51,20 @@
             </form>
         </div>
     </div>
+@endsection
+
+@section('scripts')
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/trix/1.0.0/trix.js" integrity="sha512-DYqCX8kO/IP/uf6iT0+LnI6ft5aDdONwabmbgVxjR94pwCefuJwYwd+NAsKFpH3hk8wP2L3jRn9g61t3r2N9VA==" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/flatpickr"></script>
+    <script>
+        flatpickr('#published_at', {
+            enableTime: true
+        })
+    </script>
+@endsection
+
+
+@section('css')
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/trix/1.0.0/trix.css" integrity="sha512-An9xk8CstwPHW2Vzjj0RA6Gdbi3RUkEMqocdnEtq2C/iKJLKV0JGaJTMgyn2HeolVe0zDtDhXP7OMaTSffCkqw==" crossorigin="anonymous" />
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/flatpickr/dist/flatpickr.min.css">
 @endsection
